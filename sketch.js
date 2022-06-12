b12023e5201ed9b2893ce5deac2e50d57b7b3ba4
Francky <franck@usboot.fr>
2022-06-12 09:31:21 +0200

    mef2

--- sketch.js
+++ sketch.js
@@ -1,12 +1,17 @@
 p5.disableFriendlyErrors = true;
 function setup()
 {
-  frameRate(30);
-  angleMode(DEGREES);
-  pixelDensity(1);
+  frameRate( 30 );
+  angleMode( DEGREES );
+  pixelDensity( 1 );
   z = 400;
-  createCanvas(z,z);
-  a = [ {x:0,y:-z/2},{x:z/2,y:0}, {x:0, y:z/2}, {x:-z/2, y:0} ];
+  a = [ 
+        {x:0, y:-z/2},
+        {x:z/2, y:0},
+        {x:0, y:z/2},
+        {x:-z/2, y:0}
+      ];
+  createCanvas( z ,z );
 }
 
 function draw()
@@ -15,14 +20,14 @@ function draw()
   push();
     translate(z/2,z/2)
     rotate(frameCount/2);
-    Quarter();
+    StarLine();
   pop();
 }
 
-function Quarter()
+function StarLine()
 {
-  stroke([191,201,202,255]);
-  strokeWeight(1);
+  stroke( "snow" );
+  strokeWeight( 1 );
   for( var s = 0; s < z/2; s += 20 ) {
     line( a[0].x, a[0].y + s, a[0].x + s, a[1].y );
     line( a[1].x -s, a[1].y, a[0].x, a[1].y + s );
